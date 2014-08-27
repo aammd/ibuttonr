85afb4aaa8679bd20fe1137532cdb4ea4b9e009b
Andrew <a.a.m.macdonald@gmail.com>
2014-08-27 13:23:56 -0700

    package skeleton added

---
+++ R/ibuttonr.R
@@ -0,0 +1,10 @@
+#' Read data from ibuttons.
+#'
+#' ibuttonr contains some convenience functions for loading
+#' data from ibuttons
+#'
+#' The functions read, clean and check the quality of your data
+#'
+#' @docType package
+#' @name ibuttonr
+NULL
