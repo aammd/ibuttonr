32394e44af8c69a2e717a46f9a2eee464f52eba5
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-05-07 11:14:50 -0700

    testing read_ibutton_folder :persevere:

--- tests/testthat/test_reading.R
+++ tests/testthat/test_reading.R
@@ -11,5 +11,16 @@ test_that("ibuttonr reads data correctly", {
   ibut <- read_named_ibutton(data)
   expect_equal(class(ibut), "data.frame")
 
+  foldername <- gsub(x = data, pattern = "12a.csv", replacement = "")
+
+  ibutlist <- read_ibutton_folder(foldername)
+
+  expect_equal(class(ibutlist), "list")
+
+  expect_equal(length(ibutlist), 1)
+  # name of list element
+  expect_equal(names(ibutlist), "12a")
+  # name of columns
+  expect_equal(names(ibutlist[[1]]), c("ibutton", "Date.Time", "Unit", "Value"))
 
 })
